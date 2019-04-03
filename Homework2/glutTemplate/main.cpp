c94dc0f53aaf973f1585cf01b9dcbe50189babb8
Park, Cheolhyeong <parkcheolhyeong@gmail.com>
2019-04-04 00:16:29 +0900

    Add Method of drawing_vertical_horizon

--- Homework2/glutTemplate/main.cpp
+++ Homework2/glutTemplate/main.cpp
@@ -3,22 +3,25 @@
 #define CHEIGHT 600.f
 #define CWIDTH 800.f
 
-int winID;
 
 /* Structure Type of Point */
 typedef struct Point {
 	int x, y;
 } Point;
 
-/* Structure Type of Line */
-typedef struct Line {
-	int p0, p1;
-} Line;
-
 /* Structure Type of Color */
 typedef struct Color {
 	unsigned char r, g, b;
 } Color;
+
+int winID;
+
+// Init Points, Color
+Point points[10] = { {0, 0}, {-300, 200},{-300, -200},
+					{-100, -150}, {-100, 150},
+					{300, 0}, {300, 200},
+					{100, 200}, {100, 0}, {100, -200} };
+Color b = { 0, 0, 0 };
 
 /* Convert from Camera To View*/
 Point CameraToViewport(const Point &p)
@@ -30,17 +33,51 @@ Point CameraToViewport(const Point &p)
 	return vPoint;
 }
 
-/* Draw to Line (Method of Normalized Vector) */
+void DrawHLine(Point p1, Point p2, Color c)
+{
+
+}
+
+
+/* Draw to Line (Method of Normalized Vector, etc) */
 void DrawLine(Point p1, Point p2, Color c) {
+	/* Draw to Line (Horizon) */
+	if (p1.y - p2.y == 0)
+	{
+		if (p1.x < p2.x)
+		{
+			//p1부터 그린다.
+			for (int i = p1.x; i < p2.x; i++)
+				FrameBuffer::SetPixel(i, p1.y, c.r, c.g, c.b);
+		}
+		else {
+			//p2부터 그린다.
+			for (int i = p2.x; i < p1.x; i++)
+				FrameBuffer::SetPixel(i, p2.y, c.r, c.g, c.b);
+		}
+	}
+	/* Draw to Line (Vertical) */
+	else if (p1.x - p2.x == 0) {
+		if (p1.y < p2.y)
+		{
+			//p1부터 그린다.
+			for (int i = p1.y; i < p2.y; i++)
+				FrameBuffer::SetPixel(p1.x, i, c.r, c.g, c.b);
+		}
+		else {
+			//p2부터 그린다.
+			for (int i = p2.y; i < p1.y; i++)
+				FrameBuffer::SetPixel(p2.x, i, c.r, c.g, c.b);
+		}
+	} else { 	// Draw to Line (Normalized vector)
+		float vectorSize = sqrt(pow(p1.y - p2.y, 2) + pow(p1.x - p2.x, 2));		// Distance of point to point
 
-	float vectorSize = sqrt(pow(p1.y - p2.y, 2) + pow(p1.x - p2.x, 2));		// Distance of point to point
+		float UVectorx = (p1.x - p2.x) / vectorSize;		// Unit vector x
+		float UVectory = (p1.y - p2.y) / vectorSize;		// Unit vector y
 
-	float UVectorx = (p1.x - p2.x) / vectorSize;		// Unit vector x
-	float UVectory = (p1.y - p2.y) / vectorSize;		// Unit vector y
-
-	// Draw
-	for (int i = 0; i < vectorSize; i++) {
-		FrameBuffer::SetPixel(p2.x + (i*UVectorx), p2.y + (i*UVectory), c.r, c.g, c.b);
+		for (int i = 0; i < vectorSize; i++) {
+			FrameBuffer::SetPixel(p2.x + (i*UVectorx), p2.y + (i*UVectory), c.r, c.g, c.b);
+		}
 	}
 }
 
@@ -73,25 +110,10 @@ void loop(void)
 
 void render(void)
 {
-	int i;
 	FrameBuffer::Clear(255, 255, 255);
 
 	//Put your rendering code here
 
-	// Init Points, Color
-	Point points[] = { {0, 0}, {-300, 200},{-300, -200},
-						{-100, -150}, {-100, 150},
-						{300, 0}, {300, 200},
-						{100, 200}, {100, 0}, {100, -200} };
-	Color b = { 0, 0, 0 };
-
-	// Convert from Camerat to View
-	for (i = 1; i < 10; i++)
-	{
-		points[i] = CameraToViewport(points[i]);
-		printf("%d = %d, %d\n", i, points[i].x, points[i].y);
-	}
-	
 	// Draw figure
 	DrawLine(points[1], points[2], b);
 	DrawLine(points[2], points[3], b);
@@ -112,8 +134,16 @@ void render(void)
 void init(void)
 {
 	FrameBuffer::Init(WIDTH, HEIGHT);
+	int i = 0;
+	printf("Goal: Camera(%d X %d) -> View (%d X %d)\n\n\n", (int)CWIDTH, (int)CHEIGHT, (int)WIDTH, (int)HEIGHT);
 
-	//Initialize everything here
+	// Convert from Camerat to View
+	for (i = 1; i < 10; i++)
+	{
+		printf("P_c[%4d](%4d, %4d) -> ", i, points[i].x, points[i].y);
+		points[i] = CameraToViewport(points[i]);
+		printf("P_v[%4d](%4d, %4d)\n", i, points[i].x, points[i].y);
+	}
 }
 
 int main (int argc, char **argv)
