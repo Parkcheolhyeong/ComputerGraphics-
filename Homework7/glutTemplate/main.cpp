28e626bf609d041fad375cba05f7ba00a1f76559
parkch1124 <parkcheolhyeong@gmail.com>
2019-05-21 00:37:19 +0900

    Completed "Ellipse Draw"

--- Homework7/glutTemplate/main.cpp
+++ Homework7/glutTemplate/main.cpp
@@ -14,58 +14,109 @@ typedef struct Color {
 	unsigned char r, g, b;
 } Color;
 
-
+Color Black = {0, 0, 0};
 int winID;	// window's name
 int flagDoubleDraw = 1;	// flag to prevent double draw
 int circleState = 1;	// flag to active key 'c'
-Point centerPoint = { 0, 0 }, radiusPoint = { 0, 0 };	// Point of center, radius
+Point pC = { 0, 0 }, pX = { 0, 0 }, pY = { 0, 0 };	// Point of center, x, y
 
 /* EightWaySymmetric */
-void DrawCircleEightWaySymmetric(int x, int y, int xC, int yC)
+void DrawCircleFourWaySymmetric(int x, int y, int xC, int yC)
 {
-	FrameBuffer::SetPixel(x + xC, -y + yC, 0, 0, 0);
 	FrameBuffer::SetPixel(x + xC, y + yC, 0, 0, 0);
-	FrameBuffer::SetPixel(-x + xC, -y + yC, 0, 0, 0);
 	FrameBuffer::SetPixel(-x + xC, y + yC, 0, 0, 0);
-	FrameBuffer::SetPixel(y + xC, -x + yC, 0, 0, 0);
-	FrameBuffer::SetPixel(y + xC, x + yC, 0, 0, 0);
-	FrameBuffer::SetPixel(-y + xC, -x + yC, 0, 0, 0);
-	FrameBuffer::SetPixel(-y + xC, x + yC, 0, 0, 0);
+	FrameBuffer::SetPixel(x + xC, -y + yC, 0, 0, 0);
+	FrameBuffer::SetPixel(-x + xC, -y + yC, 0, 0, 0);
 }
 
-/* Midpoint CIrcle Drawing */
-void MidpointCircleDraw(int xC, int yC, float radius)
+/* Midpoint Ellipse Drawing */
+void MidpointEllipseDraw(int xC, int yC, float a, float b)
 {
 	/* Init x, y */
-	int x = 0;
-	int y = round(radius);
-
+	int x, y;
+	float d1, dE1, dSE1;
+	float d2, dS2, dSE2;
+	int powA, powB;
+	int x0 = pow(a, 2) / sqrt(pow(a, 2) + pow(b, 2));
+	int y0 = pow(b, 2) / sqrt(pow(a, 2) + pow(b, 2));
+	x = 0;
+	y = round(b);
+	powA = pow(a,2); powB = pow(b,2);
 	/* Init decision variables */
-	float d = (float)(5 / 4) - radius;
-	float dE = (2 * x) + 3;
-	float dSE = (2 * (x - y)) + 5;
+	d1 = powB - (powA * b) + (powA / 4);
+	dE1 = (3 * powB);
+	dSE1 = (3 * powB) - (2 * powA * b) + (2 * powA);
 
 	/* Init xStep, yStep */
 	int xStep = 1;
 	int yStep = 1;
 
-	while (x<=y) {
-		if (d > 0) {	// Outside of Circle
+	while (x<= x0) {
+
+
+		if (d1 > 0) {	// Outside of Circle
 		/* Update decision variables and coordinate*/
-		d += dSE;
-		dE += 2.f; dSE += 4.f;
-
-		x += xStep;
-		y -= yStep;
-
-		DrawCircleEightWaySymmetric(x, y, xC, yC);	// Drawing Eight Way
-		} else if (d <= 0) {	// Inside of CIrcle or Boundary of Circle
-			/* Update decision variables and coordinate*/
-			d += dE;
-			dE += 2.f; dSE += 2.f;
 			x += xStep;
-			DrawCircleEightWaySymmetric(x, y, xC, yC);	// Drawing Eight Way
-		} 
+			y -= yStep;
+			FrameBuffer::SetPixel(x + xC, y + yC, 0, 0, 0);
+			FrameBuffer::SetPixel(-x + xC, y + yC, 0, 0, 0);
+			FrameBuffer::SetPixel(x + xC, -y + yC, 0, 0, 0);
+			FrameBuffer::SetPixel(-x + xC, -y + yC, 0, 0, 0);
+	
+			d1 += dSE1;
+			dE1 += 2 * powB;
+			dSE1 += ((2 * powB) + (2 * powA));
+		} else if (d1 <= 0) {	// Inside of CIrcle or Boundary of Circle
+			/* Update decision variables and coordinate*/
+			x += xStep;
+			FrameBuffer::SetPixel(x + xC, y + yC, 0, 0, 0);
+			FrameBuffer::SetPixel(-x + xC, y + yC, 0, 0, 0);
+			FrameBuffer::SetPixel(x + xC, -y + yC, 0, 0, 0);
+			FrameBuffer::SetPixel(-x + xC, -y + yC, 0, 0, 0);
+
+			d1 += dE1;
+			dE1 += 2 * powB;
+			dSE1 += 2 * powB;
+
+		}
+		
+
+	}
+
+	/* Init decision variables */
+	d2 = (powB*x) - (2 * powA * y) + powA + (powB / 4);
+	dS2 = -(2 * powA*y) + (3 * powA);
+	dSE2 = (2 * powB* x) - (2 * powA * y) + (3 * powA);
+	while (y >= 0) {
+
+		/* Init x, y */
+
+		if (d2 > 0) {
+			y -= yStep;
+
+			FrameBuffer::SetPixel(x + xC, y + yC, 0, 0, 0);
+			FrameBuffer::SetPixel(-x + xC, y + yC, 0, 0, 0);
+			FrameBuffer::SetPixel(x + xC, -y + yC, 0, 0, 0);
+			FrameBuffer::SetPixel(-x + xC, -y + yC, 0, 0, 0);
+			d2 += dS2;
+			dS2 += 2 * powA;
+			dSE2 += 2 * powA;
+
+		}
+		else if (d2 <= 0) {	// Inside of CIrcle or Boundary of Circle
+		   /* Update decision variables and coordinate*/
+			y -= yStep;
+			x += xStep;
+			FrameBuffer::SetPixel(x + xC, y + yC, 0, 0, 0);
+			FrameBuffer::SetPixel(-x + xC, y + yC, 0, 0, 0);
+			FrameBuffer::SetPixel(x + xC, -y + yC, 0, 0, 0);
+			FrameBuffer::SetPixel(-x + xC, -y + yC, 0, 0, 0);
+			d2 += dSE2;
+			dS2 += 2 * powA;
+			dSE2 += (2 * powB) + (2 * powA);
+
+		}
+		
 	}
 		
 }
@@ -74,7 +125,7 @@ void keyboard(unsigned char key, int x, int y)
 {
 	switch (key)
 	{
-	case 'c':
+	case 'e':
 		circleState = 0; flagDoubleDraw = 1;	// Init flag variables
 		break;
 	case 'r':
@@ -96,37 +147,40 @@ void mouse(int button, int state, int x, int y)
 			printf("1\n");
 			printf("%d, %d\n", x, y);
 
-			centerPoint.x = x;
-			centerPoint.y = y;
+			pC.x = x;
+			pC.y = y;
 			flagDoubleDraw = 0;
 		}
 		break;
 
-	case (GLUT_RIGHT_BUTTON):
+	case (GLUT_RIGHT_BUTTON):	// Value of b
 		if (state == GLUT_DOWN)	// Save radiusPoint & Drawing Circle & Set flagDoubleDraw
 		{
-			if (circleState == 0 && flagDoubleDraw == 0)
-			{
-				float r = 0.f;
-				
-				radiusPoint.x = x;
-				radiusPoint.y = y;
+			//if (circleState == 0 && flagDoubleDraw == 0)
+			//{
+				pY.x = x;
+				pY.y = y;
 				printf("2\n");
 				printf("%d, %d\n", x, y);
-
-				r = sqrt(pow(radiusPoint.x - centerPoint.x, 2) + pow(radiusPoint.y - centerPoint.y, 2));	// Calc radius
-
-
-				MidpointCircleDraw(centerPoint.x, centerPoint.y, r);
+				
+				float rX = sqrt(pow(pX.x - pC.x, 2) + pow(pX.y - pC.y, 2));
+				float rY = sqrt(pow(pY.x - pC.x, 2) + pow(pY.y - pC.y, 2));
+				printf("%f, %f\n", rX, rY);
+				MidpointEllipseDraw(pC.x, pC.y, rX, rY);
 				flagDoubleDraw = 1;
-			}
+			//}
 		}
 		break;
 
-	case (GLUT_MIDDLE_BUTTON):
+	case (GLUT_MIDDLE_BUTTON):	// Value of a
 		if (state == GLUT_DOWN)
 		{
+			pX.x = x;
+			pX.y = y;
+
 			printf("3\n");
+			printf("%d, %d\n", x, y);
+
 		}
 		break;
 	}	
